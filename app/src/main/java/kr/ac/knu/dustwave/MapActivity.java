a8007b2853e5f4e8e9dc81aa989ec658d22cc758
nicewoong <thoughtscode@gmail.com>
2017-06-17 23:40:27 +0900

    Deleted Daum API Library from project

--- app/src/main/java/kr/ac/knu/dustwave/MapActivity.java
+++ app/src/main/java/kr/ac/knu/dustwave/MapActivity.java
@@ -2,6 +2,8 @@ package kr.ac.knu.dustwave;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,6 +11,17 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        createMapView(); // create map view using DAUM map api
+
+    }
+
+
+    /**
+     * create map view using DAUM map api
+     */
+    public void createMapView() {
+
     }
 
 }
