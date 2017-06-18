8016f6f81ff14786eea54f16b27f6461b23651c4
nicewoong <thoughtscode@gmail.com>
2017-06-18 10:28:21 +0900

    Add setting the center pointer on the DAUM map @dMapActivity
    - dMapActivity 에서 지도의 중심을 설정하는 method 추가
    - 지도 생성 후 각종 interface 를  implements  했음.

--- app/src/main/java/kr/ac/knu/dustwave/DMapActivity.java
+++ app/src/main/java/kr/ac/knu/dustwave/DMapActivity.java
@@ -4,20 +4,142 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
-public class DMapActivity extends AppCompatActivity {
+
+/**
+ * 다음지도API를 활용해서 지도 화면을 보여주는 Activity.
+ */
+
+public class DMapActivity extends AppCompatActivity implements MapView.MapViewEventListener,  MapView.POIItemEventListener  {
+
+    MapView mapView;
+    ViewGroup mapViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dmap);
 
-        MapView mapView = new MapView(this);
+
+        createMapView(); // 다음 지도를 생성해서 view 를 채운다.
+
+
+    }
+
+    /**
+     * 다음지도API를 활용해서 맵뷰를 생성해서 activity 를 채운다.
+     *
+     */
+    public void createMapView() {
+        mapView = new MapView(this);
         mapView.setDaumMapApiKey(this.getResources().getString(R.string.daum_map_view_api_key)); // 다음 api key 를 인자로 넘겨준다.
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        //이벤트리스너 등록
+        mapView.setMapViewEventListener(this); // this에 MapView.MapViewEventListener 구현.
+        mapView.setPOIItemEventListener(this);
+
+
+    }
+
+
+    /**
+     * 지도의 중심을 어디로 해서 보여줄지 설정
+     * todo 사용자의 현재위치를 받아서 중심점을 선택하는 기능으로 변경하기
+     *
+     */
+    public void setMapCenter() {
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.8714354, 128.601445), true); // 대구광역시
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(7, true);
+
+//        // 중심점 변경 + 줌 레벨 변경
+//        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
+
+        // 줌 인
+        mapView.zoomIn(true);
+
+        // 줌 아웃
+        mapView.zoomOut(true);
+    }
+
+
+
+    // ============ MapView.MapViewEventListener 구현해야하는 메서드 ============ //
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        setMapCenter(); //지도의 중심점 설정
+    }
+
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+
+    // ============ MapView.POIItemEventListener 구현해야하는 메서드 ============ //
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
 
     }
 }
