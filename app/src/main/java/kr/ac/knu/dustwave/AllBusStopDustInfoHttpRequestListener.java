415482a4f44a9a6670000d374172a6c68a0a21c9
nicewoong <thoughtscode@gmail.com>
2017-06-22 00:25:16 +0900

    Added CurrentlocationDustInfo class, interface listener
    - 현재 위치를 기반으로 해당 위치의 미세먼지 정보를 받아오고 처리하는 것을 담당하는 클래스 추가
    - 데이터를 http 통신으로 다 받아왔을 때 호출되는 리스너 인터페이스 추가

---
+++ app/src/main/java/kr/ac/knu/dustwave/AllBusStopDustInfoHttpRequestListener.java
@@ -0,0 +1,9 @@
+package kr.ac.knu.dustwave;
+
+/**
+ * Created by nicewoong on 2017. 6. 22..
+ */
+
+public interface AllBusStopDustInfoHttpRequestListener {
+    void onAllBusStopDustInfoDataStringArrived(String requestStream);
+}
---
+++ app/src/main/java/kr/ac/knu/dustwave/CurrentLocationDustInfoHttpRequestListener.java
@@ -0,0 +1,10 @@
+package kr.ac.knu.dustwave;
+
+/**
+ * Created by nicewoong on 2017. 6. 21..
+ */
+
+public interface CurrentLocationDustInfoHttpRequestListener {
+
+    void onCurrentLocationDataStringArrived(String requestStream);
+}
