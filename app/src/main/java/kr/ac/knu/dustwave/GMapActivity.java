56d23956126c67167e4b6337296fb98cfe1c5e79
nicewoong <thoughtscode@gmail.com>
2017-06-22 22:39:35 +0900

    Created Google Map Activity and Added meta data to manifest

---
+++ app/src/main/java/kr/ac/knu/dustwave/GMapActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.knu.dustwave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goole_map);
+    }
+}
