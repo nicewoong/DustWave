21608322de0d37504662860be613af17dd5b8e04
nicewoong <thoughtscode@gmail.com>
2017-06-23 09:53:32 +0900

    Modify circle overlay size

--- app/src/main/java/kr/ac/knu/dustwave/GMapActivity.java
+++ app/src/main/java/kr/ac/knu/dustwave/GMapActivity.java
@@ -1,7 +1,6 @@
 package kr.ac.knu.dustwave;
 
 import android.graphics.Color;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,9 +15,6 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
-
-import net.daum.mf.map.api.MapPOIItem;
-import net.daum.mf.map.api.MapPoint;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -55,7 +51,7 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
 
         // 카메라 중심 이동
         googleMap.moveCamera(CameraUpdateFactory.
-                newLatLngZoom(new LatLng(MainActivity.latestLatitude, MainActivity.latestLongitude), 17)); // 숫자 높을 수록 확대
+                newLatLngZoom(new LatLng(MainActivity.latestLatitude, MainActivity.latestLongitude), 15)); // 숫자 높을 수록 확대
 
         // 현재 위치 (중점) 마커 표시
         addCurrentLocationMarker(new LatLng(MainActivity.latestLatitude, MainActivity.latestLongitude), googleMap);
@@ -181,7 +177,7 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
 
         for(int i = 0; i< busStopDustInfoList.length(); i=i+2) {
             try {
-                addCustomDustInfoClusterMarker(busStopDustInfoList.getJSONObject(i), googleMap); // 마커그리기
+                addDustInfoClusterMarker(busStopDustInfoList.getJSONObject(i), googleMap); // 마커그리기
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -196,7 +192,7 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
      * 지도 위에 지정된 위치에 maker 를 표시합니다.
      *
      */
-    public void addCustomDustInfoClusterMarker(JSONObject dustInfoObject, GoogleMap googleMap) {
+    public void addDustInfoClusterMarker(JSONObject dustInfoObject, GoogleMap googleMap) {
 
         double dustInfoPm10; //미세먼지
         double dustInfoPm25; //초미세먼지
@@ -204,8 +200,6 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
         double longitude;
         String fineDustLevel; //좋은지 나쁜지 text
         int markerResourceId;
-
-
 
         try {
             dustInfoPm10 = dustInfoObject.getDouble(LocalDatabaseKey.dust_info_pm10);
@@ -217,31 +211,6 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
             BusStopClusterItem busStopClusterItem = new BusStopClusterItem(new LatLng(latitude, longitude));
             mClusterManager.addItem(busStopClusterItem);
 
-
-
-//            if (dustInfoPm10 <= 30) {
-//                fineDustLevel = "좋음"; //파랑색
-//                markerResourceId = R.drawable.marker_green;
-//            } else if (dustInfoPm10 > 30 && dustInfoPm10 <= 80) {
-//                fineDustLevel = "보통"; //초록
-//                markerResourceId = R.drawable.marker_blue;
-//
-//            } else if (dustInfoPm10 > 80 && dustInfoPm10 <= 150) {
-//                fineDustLevel = "나쁨"; //오린지
-//                markerResourceId = R.drawable.marker_orange;
-//
-//            } else {
-//                fineDustLevel = "매우나쁨"; //빨강
-//                markerResourceId = R.drawable.marker_red;
-//
-//            }
-//
-//            // 마커 추가
-//            googleMap.addMarker(new MarkerOptions()
-//                    .position(new LatLng(latitude, longitude))
-//                    .title(fineDustLevel)
-//                    .alpha(0.6f) // 0 이 완전 투명
-//                    .icon(BitmapDescriptorFactory.fromResource(markerResourceId)));
 
 
         } catch (JSONException e) {
@@ -304,25 +273,25 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
 
 
             if (dustInfoPm10 <= 30) { //파랑
-                circleColor = Color.argb(50, 0, 0, 255);
+                circleColor = Color.argb(150, 0, 0, 255);
 
             } else if (dustInfoPm10 > 30 && dustInfoPm10 <= 80) { //초롱
-                circleColor = Color.argb(50, 0, 255, 0);
+                circleColor = Color.argb(150, 0, 255, 0);
 
 
             }else if (dustInfoPm10 > 80 && dustInfoPm10 <= 150) { //오렌지
-                circleColor = Color.argb(50, 255,165,0);
+                circleColor = Color.argb(150, 255,165,0);
 
 
             } else{ //빨강
-                circleColor = Color.argb(50, 255, 0, 0);
+                circleColor = Color.argb(150, 255, 0, 0);
 
             }
 
             // 우선 서클옵션을 만들고
             CircleOptions circleOptions = new CircleOptions()
                     .center(new LatLng(latitude, longitude))
-                    .radius(50)
+                    .radius(150)
                     .strokeWidth(0)
                     .fillColor(circleColor); // 수치에 따른 색상
 
