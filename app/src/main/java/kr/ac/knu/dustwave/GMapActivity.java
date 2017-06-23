c68170aa72fe2a35474178a905a2ec9c4c0ecc50
nicewoong <thoughtscode@gmail.com>
2017-06-23 09:39:39 +0900

    Add circle overlay on map
    - 모든 버스 정류장 미세먼지 데이터에 대한 색상으로 circle ovelay를 추가하는 메서드 작성 완

--- app/src/main/java/kr/ac/knu/dustwave/GMapActivity.java
+++ app/src/main/java/kr/ac/knu/dustwave/GMapActivity.java
@@ -1,5 +1,6 @@
 package kr.ac.knu.dustwave;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
@@ -61,11 +64,13 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
         googleMap.setOnCameraIdleListener(mClusterManager);
 
 
-        // 받아온 모든 정류장 미세먼지 데이터 (마커만)
+        // 받아온 모든 정류장 미세먼지 데이터 (커스텀 마커만 not Clustering)
 //        addAllBusStopMarker(MainActivity.allBusStopDustInfoList, googleMap);
 
         // 받아온 모든 정류장 미세먼지 데이터 (클러스터 마커로 )
         addAllBusStopClusterMarker(MainActivity.allBusStopDustInfoList, googleMap);
+        // 받아온 모든 정류장 미세먼지 데이터를 Circle overlay 로 표시
+        addAllBusStopCircle(MainActivity.allBusStopDustInfoList, googleMap);
 
     }
 
@@ -84,6 +89,7 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_current_pink)));
 
     }
+
 
     public void addAllBusStopMarker(JSONArray busStopDustInfoList, GoogleMap googleMap) {
 
@@ -202,9 +208,7 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
 
 
         try {
-            dustInfoPm10 = Math.random() * 200 + 1;
             dustInfoPm10 = dustInfoObject.getDouble(LocalDatabaseKey.dust_info_pm10);
-
             dustInfoPm25 = dustInfoObject.getDouble(LocalDatabaseKey.dust_info_pm25);
             latitude = dustInfoObject.getDouble(LocalDatabaseKey.bus_stop_latitude);
             longitude = dustInfoObject.getDouble(LocalDatabaseKey.bus_stop_longitude);
@@ -248,6 +252,91 @@ public class GMapActivity  extends AppCompatActivity implements OnMapReadyCallba
     }
 
 
+    /**
+     * addAllBusStopMarker 와 같은 기능을 하는 클러스터용 메서드 구현
+     *
+     * @param busStopDustInfoList
+     * @param googleMap
+     */
+    public void addAllBusStopCircle(JSONArray busStopDustInfoList, GoogleMap googleMap) {
+
+        Log.d("ADD_ALL_Circle", " 모든 마커 반복문 시작! ");
+        if(busStopDustInfoList == null)
+            return;
+
+        for(int i = 0; i< busStopDustInfoList.length(); i=i+2) {
+            try {
+                addDustInfoCircleOverlay(busStopDustInfoList.getJSONObject(i), googleMap); // 마커그리기
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.d("ADD_ALL_Circle", " 모든 마커 반복문 끝! ");
+
+    }
+
+
+
+
+    /**
+     * 해당 버스스탑의 미세먼지 수치에 따른 색상으로 circle overlay를 그려줍니다.
+     *
+     * @param dustInfoObject
+     * @param googleMap
+     */
+    public void addDustInfoCircleOverlay(JSONObject dustInfoObject, GoogleMap googleMap) {
+
+        double dustInfoPm10; //미세먼지
+        double dustInfoPm25; //초미세먼지
+        double latitude;
+        double longitude;
+        String fineDustLevel; //좋은지 나쁜지 text
+        int markerResourceId;
+        int circleColor;
+
+        try {
+            dustInfoPm10 = Math.random()*200+1; // 다양한 색상을 위한 랜덤값!
+//            dustInfoPm10 = dustInfoObject.getDouble(LocalDatabaseKey.dust_info_pm10); // 진짜 값
+            dustInfoPm25 = dustInfoObject.getDouble(LocalDatabaseKey.dust_info_pm25);
+            latitude = dustInfoObject.getDouble(LocalDatabaseKey.bus_stop_latitude);
+            longitude = dustInfoObject.getDouble(LocalDatabaseKey.bus_stop_longitude);
+
+
+            if (dustInfoPm10 <= 30) { //파랑
+                circleColor = Color.argb(50, 0, 0, 255);
+
+            } else if (dustInfoPm10 > 30 && dustInfoPm10 <= 80) { //초롱
+                circleColor = Color.argb(50, 0, 255, 0);
+
+
+            }else if (dustInfoPm10 > 80 && dustInfoPm10 <= 150) { //오렌지
+                circleColor = Color.argb(50, 255,165,0);
+
+
+            } else{ //빨강
+                circleColor = Color.argb(50, 255, 0, 0);
+
+            }
+
+            // 우선 서클옵션을 만들고
+            CircleOptions circleOptions = new CircleOptions()
+                    .center(new LatLng(latitude, longitude))
+                    .radius(50)
+                    .strokeWidth(0)
+                    .fillColor(circleColor); // 수치에 따른 색상
+
+            //옵션을 활용해 써클을 추가합니다
+            Circle circle = googleMap.addCircle(circleOptions); // return 되는 circle 은 나중에 변경가능 (Mutable)
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 
 
